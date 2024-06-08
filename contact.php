943c3fd986557c49913c89dd350b213240907c85
fvinci14 <francovinci0@gmail.com>
2024-06-08 14:44:20 -0300

    cambios
    s

--- contact.php
+++ contact.php
@@ -1,10 +1,5 @@
 <?php
-    /*
-    name
-    email
-    message
-    */
-    // Only process POST reqeusts.
+    // Only process POST requests.
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
         // Get the form fields and remove whitespace.
         $name = strip_tags(trim($_POST["name"]));
@@ -12,35 +7,36 @@
         $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
         $message = trim($_POST["message"]);
 
-        
-
         // Set the recipient email address.
         // FIXME: Update this to your desired email address.
         $recipient = "modelo.onuregional@gmail.com";
 
         // Build the email content.
-        $email_content = "Name $name\n";
-        $email_content .= "Email \n$message\n";
-        $email_content .= "Message \n$message\n";
+        $email_content = "Name: $name\n";
+        $email_content .= "Email: $email\n";
+        $email_content .= "Message: $message\n";
 
         // Build the email headers.
         $email_headers = "From: $name <$email>";
 
         // Send the email.
-        if (mail($recipient,  $email_content, $email_headers)) {
+        if (mail($recipient, "New contact from $name", $email_content, $email_headers)) {
             // Set a 200 (okay) response code.
             http_response_code(200);
             echo "Thank You! Your message has been sent.";
         } else {
             // Set a 500 (internal server error) response code.
             http_response_code(500);
-            echo "Oops! Something went wrong ande we couldn't send your message.";
+            echo "Oops! Something went wrong and we couldn't send your message.";
         }
 
     } else {
         // Not a POST request, set a 403 (forbidden) response code.
         http_response_code(403);
         echo "There was a problem with your submission, please try again.";
+    }
+?>
+mission, please try again.";
     }
 
 ?>
