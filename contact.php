edeead9361f0ec317f307fdaa46af97ffacd1e78
fvinci14 <francovinci0@gmail.com>
2024-06-09 11:58:50 -0300

    .
    .

--- contact.php
+++ contact.php
@@ -19,8 +19,8 @@
 
         // Build the email content.
         $email_content = "Name $name\n";
-        $email_content .= "Email \n$email\n";
-        $email_content .= "Message \n$message\n";
+        $email_content .= "Email $email\n";
+        $email_content .= "Message $message\n";
 
         // Build the email headers.
         $email_headers = "From: $name <$email>";
